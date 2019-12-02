4a77c482c5d9c62ecd36270cf7a75fabd1f6790b
APM246 <50038558+APM246@users.noreply.github.com>
2019-12-02 17:37:45 +0800

    Add files via upload

---
+++ chess.bat
@@ -0,0 +1,13 @@
+cd "src\"
+javac Canvas.java
+javac Piece.java
+javac Pawn.java
+javac Rook.java
+javac Knight.java
+javac Bishop.java
+javac King.java
+javac Queen.java
+javac Board.java 
+javac Viewer.java
+java Viewer
+@pause
